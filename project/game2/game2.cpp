e1a411ab88281a35fc23727958bef742a137bad8
manechkaa <mariatestova5@gmail.com>
2025-05-24 22:09:12 +0300

    create game2.cpp

--- project/game2/game2.cpp
+++ project/game2/game2.cpp
@@ -20,3 +20,47 @@ void Game::gotoxy(int x, int y) {
     coord.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
+
+void Game::Draw() {
+    system("cls");
+    for(int i = 0; i < WIDTH+2; i++)
+        std::cout << "#";
+    std::cout << '\n';
+
+    for(int y = 0; y < HEIGHT; y++) {
+        for(int x = 0; x < WIDTH; x++) {
+            if (x == 0){
+                std::cout << "#";
+            }
+
+            bool isSnake = false;
+            for(auto segment : snake.body) {
+                if(segment.first == x && segment.second == y) {
+                    std::cout << "O";
+                    isSnake = true;
+                    break;
+                }
+            }
+
+            if(!isSnake) {
+                if(x == food.first && y == food.second) {
+                    std::cout << "F";
+                }
+                else
+                    std::cout << " ";
+            }
+
+            if(x == WIDTH - 1) {
+                std::cout << "#";
+            }
+        }
+        std::cout << '\n';
+    }
+
+    for(int i = 0; i < WIDTH + 2; i++) {
+        std::cout << "#";
+    }
+    std::cout << "\n";
+
+    std::cout << "Score: " << snake.score << '\n';
+}
